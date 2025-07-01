d637c1ccc0b8e1fa1e6637a64ffdcb4e2cf56386
ayexhs <shreya.anshi04@gmail.com>
2025-07-01 13:28:09 +0530

    Fix: update test to match new homepage message

--- test_app.py
+++ test_app.py
@@ -1,6 +1,4 @@
-from app import app
-
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello" in response.data
+    assert b"Auto-deploy" in response.data
